a35887e4220f92b9256d1654caf720d0f95392e6
Kameyama92 <77086536+Kameyama92@users.noreply.github.com>
2021-02-05 19:37:13 +0900

    新規ユーザー登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :prefecture_id, :city, :address_detail, :apartment_name, :phone_number]) 
+  end
+
+
 end
